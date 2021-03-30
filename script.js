7e70904db55e279f67147b5502e62db102eaa3de
Jungwon Shin <Jungwon.shin98@gmail.com>
2021-03-29 20:49:18 -0400

    font awesome

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Product Designer", "Data Scientist", "Blogger", "Data Storyteller"],
+        strings: ["Product Designer", "Project Manager Scientist","Data Storyteller"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Product Designer", "Data Scientist", "Blogger", "Data Storyteller"],
+        strings: ["Product Designer", "Project Manager Scientist","Data Storyteller"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
